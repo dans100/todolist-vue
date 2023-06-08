9e320cac9e89d14884d1b8834b0fbff55d401524
Daniel Sulkowski <dansulk@gmail.com>
2023-06-08 12:22:55 +0200

    added TodoItem component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <li>
+    <span :class="{ 'completed-task': status }">{{ task.title }}</span>
+    <button @click="deleteTask">Usuń</button>
+    <button @click="toggleStatus">Zmień status</button>
+  </li>
+</template>
+
+<script>
+
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+    index: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      required: true,
+    },
+  },
+
+  setup(props, { emit }) {
+
+    const deleteTask = () => {
+      emit('delete-task', props.index);
+    };
+
+    const toggleStatus = () => {
+      emit('toggle-status', props.index);
+    };
+
+    return {
+      deleteTask,
+      toggleStatus,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+li {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+}
+
+.completed-task {
+  text-decoration: line-through;
+}
+
+</style>
