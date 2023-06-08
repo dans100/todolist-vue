16b8f9a2098a6377de064a97c5fbdae550adcdfc
Daniel Sulkowski <dansulk@gmail.com>
2023-06-08 14:53:01 +0200

    added icons from fontawesome and styles

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,8 +1,8 @@
 <template>
   <li>
     <span :class="{ 'completed-task': status }">{{ task.title }}</span>
-    <button @click="deleteTask">Usuń</button>
-    <button @click="toggleStatus">Zmień status</button>
+    <button class="trash" @click="deleteTask"><i class="fa fa-trash"></i></button>
+    <button :class="['check', {'completed-check': status}]" @click="toggleStatus"><i class="fa fa-circle-check"></i></button>
   </li>
 </template>
 
@@ -43,15 +43,60 @@ export default {
 </script>
 
 <style scoped>
+@import '@fortawesome/fontawesome-free/css/all.css';
 
 li {
+  position: relative;
   display: flex;
   align-items: center;
   margin-bottom: 0.5rem;
+  width: 100%;
+}
+
+span {
+  display: inline-block;
+  margin: 0 auto;
+  overflow: hidden;
+  width: 50%;
+  background-color: #3E3364;
+  border-radius: 12px;
+  line-height: 40px;
+  color: white;
+  border: 2px solid #1E1A3C;
+  word-break: break-word;
 }
 
 .completed-task {
   text-decoration: line-through;
+  background-color: green;
+}
+
+button {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 1.8rem;
+  border: none;
+  background-color: #1E1A3C;
+  cursor: pointer;
+  transition: .2s;
+  color: white;
+}
+
+button:hover {
+  transform: translateY(-50%) scale(1.2);
+}
+
+.trash {
+ right: 15%;
+}
+
+.check {
+  right: 10%;
+}
+
+.completed-check {
+  color: green;
 }
 
 </style>
