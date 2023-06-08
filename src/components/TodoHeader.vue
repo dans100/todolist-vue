01563a9d92c67b7b2c9540a3025aa8a8b95f46df
Daniel Sulkowski <dansulk@gmail.com>
2023-06-08 14:52:22 +0200

    changed description in header and corrected styles

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,31 +1,25 @@
 <template>
-  <h1>To Do List</h1>
+  <h1>Lista Zadań</h1>
 </template>
 
 <style scoped>
 h1 {
-  position: absolute;
+  padding: 2rem 0;
   color: white;
-  top: 10vh;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  text-align: center;
   text-transform: uppercase;
   font-weight: 300;
   letter-spacing: 2px;
-  padding: 5px;
   animation: slide-down 300ms ease-out forwards;
-  animation-delay: 300ms;
   opacity: 0;
 }
 
 @keyframes slide-down {
   from {
     opacity: 0;
-    transform: translate(-50%, -60%);
   }
   to {
     opacity: 75%;
-    transform: translate(-50%, -50%);
   }
 }
 
