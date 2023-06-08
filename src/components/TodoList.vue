3cdb3650e4f440a33b7afc9c59605e46b855d457
Daniel Sulkowski <dansulk@gmail.com>
2023-06-08 14:54:14 +0200

    added optional text when todo list is empty and added styles

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,8 @@
 <template>
+  <TodoHeader />
   <div>
-    <TodoHeader />
     <TodoForm @add-task="handleAddTask" />
-    <ul>
+    <ul v-if="tasks.length > 0">
       <TodoItem
           v-for="(task, index) in tasks"
           :key="index"
@@ -13,6 +13,7 @@
           @toggle-status="toggleTaskStatus"
       />
     </ul>
+    <p v-else>Lista zadań jest pusta</p>
   </div>
 </template>
 
@@ -52,8 +53,35 @@ export default {
 
 <style scoped>
 
+div {
+  margin: 10vh auto;
+  width: 80%;
+  max-width: 800px;
+  box-shadow: 0 1px 8px black;
+  background-color: #1E1A3C;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  color: #fff;
+  padding: 20px 10px;
+  opacity: 75%;
+  animation: slide-down 300ms ease-out forwards;
+}
+
 ul {
   list-style: none;
+  padding: 0;
+}
+
+@keyframes slide-down {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 75%;
+  }
 }
 
 </style>
