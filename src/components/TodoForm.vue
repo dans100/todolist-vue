dbf4dd5442fe4092a744f889dda2702bbabdc437
Daniel Sulkowski <dansulk@gmail.com>
2023-06-08 11:03:17 +0200

    added TodoForm component

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="addTask">
+    <input type="text" v-model="newTask" placeholder="Dodaj nowe zadanie" />
+    <button type="submit">Dodaj</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  emits: ['add-task'],
+
+  setup(props, {emit}) {
+    const newTask = ref('');
+
+    const addTask = () => {
+      if (newTask.value.trim() !== '') {
+        emit('add-task', newTask.value);
+        newTask.value = '';
+      }
+    };
+
+    return {
+      newTask,
+      addTask,
+    };
+  },
+};
+</script>
